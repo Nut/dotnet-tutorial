cdc3e8593c7286a70e6e99219b40902eb91627b1
Yannick Stephan <yannick_stephan@t-online.de>
2022-07-24 17:46:19 +0200

    add HTTP client

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,17 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+
 builder.Services.AddControllers();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Console.WriteLine($"--> CommandService Endpoint {Configuration["CommandService"]}");
 
 var app = builder.Build();
 
